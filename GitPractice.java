375e3d3481d1674d87574c0f0c4d0018d8ddb56d
QADinesh <dineshget09@gmail.com>
2024-03-15 17:50:50 +0530

    new push commited

---
+++ GitPractice.java
@@ -0,0 +1,27 @@
+package com.gitpractice;
+
+public class GitPractice {
+	
+	private void tc1() {
+		System.out.println("tc1");
+
+	}
+	private void tc2() {
+		System.out.println("tc2");
+
+	}
+	private void tc3() {
+		System.out.println("tc3");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GitPractice gitPractice=new GitPractice();
+		gitPractice.tc1();
+		
+		
+	}
+
+}
