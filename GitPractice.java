33a6d0c2c83e2595e571f030d4a2ef28375b0a03
QADinesh <dineshget09@gmail.com>
2024-03-15 18:01:46 +0530

    after modification adding into branch

--- GitPractice.java
+++ GitPractice.java
@@ -3,15 +3,17 @@ package com.gitpractice;
 public class GitPractice {
 	
 	private void tc1() {
-		System.out.println("tc1");
+		System.out.println("tc1--->After edit");
 
 	}
 	private void tc2() {
 		System.out.println("tc2");
+		System.out.println("tc2--->After edit");
 
 	}
 	private void tc3() {
 		System.out.println("tc3");
+		System.out.println("tc3--->After edit");
 
 	}
 	
